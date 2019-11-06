845fed9f9bbe0acb8b2242e3fea8b0b4d81c7477
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-06 10:28:10 +0300

    # added .gitignore file # initial commit

---
+++ internal/scheduler.go
@@ -0,0 +1,5 @@
+package internal
+
+func GetFullName() string {
+	return "Hello GOOO!!!"
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"miya/internal"
+)
+
+func main() {
+	fmt.Println(internal.GetFullName())
+}
