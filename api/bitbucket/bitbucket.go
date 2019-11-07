332bdfdfad60763c716a45bcd03ed554d7863fa4
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-07 12:55:21 +0300

    added fetchPRs method

--- api/bitbucket/bitbucket.go
+++ api/bitbucket/bitbucket.go
@@ -1,7 +1,24 @@
 package bitbucket
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"miya/api"
+	"miya/api/bitbucket/model"
+	"miya/init/env"
+	"net/http"
+)
 
 func Listen() {
 	fmt.Println("Bitbucket-PR is listening....")
+	fetchPRs()
+}
+
+func fetchPRs() {
+	req, _ := http.NewRequest("GET", env.BitbucketFetchPrListUrl, nil)
+	req.Header.Add("Authorization", env.BitbucketToken)
+
+	page := model.Pagination{}
+	api.Send(req, &page)
+	log.Printf("Response... %s", page.ToString())
 }
