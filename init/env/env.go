6a0fa75d8d98b9f492ea534d5f2bb3aa13ccf953
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-07 13:05:47 +0300

    refactored code:   # initial environment variables ones

--- init/env/env.go
+++ init/env/env.go
@@ -4,23 +4,28 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"sync"
 )
 
 var BitbucketFetchPrListUrl = getEnv("BITBUCKET_FETCH_PR_LIST_URL", "")
 var BitbucketToken = getEnv("BITBUCKET_TOKEN", "")
 
+var doOnce sync.Once
+
 func getEnv(key string, defaultVal string) string {
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
+	loadEnv()
 
-	return getEnvFromOS(key, defaultVal)
-}
-
-func getEnvFromOS(key string, defaultVal string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
 
 	return defaultVal
+}
+
+func loadEnv() {
+	doOnce.Do(func() {
+		if err := godotenv.Load(); err != nil {
+			log.Print("No .env file found")
+		}
+	})
 }
