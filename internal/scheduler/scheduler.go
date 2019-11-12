31adc853b54430ab8904114aa0295731f28e5f9e
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-12 13:29:20 +0300

    changed scheduler time from 10 ms to 1 m

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ScheduleRemoteRepositories() {
-	_, e := scheduler.Every(10).Seconds().Run(remoterepository.ListenRemoteRepositories)
+	_, e := scheduler.Every(1).Minutes().Run(remoterepository.ListenRemoteRepositories)
 
 	if e != nil {
 		log.Printf("ScheduleRemoteRepositories:: An Error accoured to scheduler for remote repositories")
