e25fca74fe98baac899c33db5f07ec2137314503
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-12 13:09:58 +0300

    un commented update pr

--- internal/remoterepository/bitbucket/api.go
+++ internal/remoterepository/bitbucket/api.go
@@ -37,7 +37,7 @@ func updatePRs(repo *repository.Repository) {
 		req.Header.Add("Authorization", repo.Token)
 		req.Header.Add("Content-Type", "application/json")
 
-		//client.PUT(req)
+		client.PUT(req)
 
 		log.Printf("%+v\n", pr)
 	}
