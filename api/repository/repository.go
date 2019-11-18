c4a038e05a1c30a55b0b817ea0a055b2d2072afe
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-18 09:14:37 +0300

    added repository controller

---
+++ api/repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import "github.com/gin-gonic/gin"
+
+func InitRepositories(router *gin.Engine) {
+	group := router.Group("/repository")
+	group.GET("/", getRepositories)
+
+}
+
+func getRepositories(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"repositories": "repos are coming.....",
+	})
+}
