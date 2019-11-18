cab159b702706961c2255f0002bcc5044688277c
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-18 13:52:53 +0300

    added delete method

--- api/repository.go
+++ api/repository.go
@@ -11,6 +11,7 @@ func init() {
 	group := Router.Group("/repository")
 	group.POST("/", saveRepository)
 	group.GET("/", getRepositories)
+	group.DELETE("/:name", deleteRepository)
 }
 
 func saveRepository(c *gin.Context) {
@@ -27,11 +28,27 @@ func saveRepository(c *gin.Context) {
 	err = storage.Storage.PUT(repo.Name, repo)
 	if err != nil {
 		c.JSON(500, gin.H{
-			"error": err,
+			"error": err.Error(),
 		})
 	}
 
 	c.JSON(200, repo)
+}
+
+func deleteRepository(c *gin.Context) {
+	name := c.Param("name")
+
+	err := storage.Storage.Delete(name)
+	if err != nil {
+		c.JSON(404, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(204, gin.H{
+		"message": "Repository deleted ...",
+	})
+
 }
 
 func getRepositories(c *gin.Context) {
