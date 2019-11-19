952dc9b94bc66b992d683c7877ee5307db8ba6c9
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-19 15:34:38 +0300

    refactored get env

--- init/env/env.go
+++ init/env/env.go
@@ -7,19 +7,44 @@ import (
 	"sync"
 )
 
-var BitbucketFetchPrListUrl = getEnv("BITBUCKET_FETCH_PR_LIST_URL", "")
-var BitbucketToken = getEnv("BITBUCKET_TOKEN", "")
+var BitbucketFetchRepositoryUsersURL = bitbucketFetchRepoUsersURL()
+var BitbucketFetchProjectUsersURL = bitbucketFetchProjectUsersURL()
+var BitbucketFetchPRURL = bitbucketFetchPrListURL()
+var BitbucketToken = getEnv("BITBUCKET_TOKEN")
 
 var doOnce sync.Once
 
-func getEnv(key string, defaultVal string) string {
+// get project name & repo name dynamically from repo
+func bitbucketFetchRepoUsersURL() string {
+	return getEnv("BITBUCKET_BASE_URL") +
+		getEnv("BITBUCKET_PROJECT_PATH") +
+		getEnv("BITBUCKET_REPOSITORY_PATH") +
+		getEnv("BITBUCKET_USER_PATH")
+
+}
+
+// get project name dynamically from repo
+func bitbucketFetchProjectUsersURL() string {
+	return getEnv("BITBUCKET_BASE_URL") +
+		getEnv("BITBUCKET_PROJECT_PATH") +
+		getEnv("BITBUCKET_USER_PATH")
+}
+
+func bitbucketFetchPrListURL() string {
+	return getEnv("BITBUCKET_BASE_URL") +
+		getEnv("BITBUCKET_PROJECT_PATH") +
+		getEnv("BITBUCKET_REPOSITORY_PATH") +
+		getEnv("BITBUCKET_PR_PATH")
+}
+
+func getEnv(key string) string {
 	loadEnv()
 
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
 
-	return defaultVal
+	return ""
 }
 
 func loadEnv() {
