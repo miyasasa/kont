a423f2e30fdf590480ffdc4dd6547e5793bb69f3
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-19 16:10:06 +0300

    added initialize function to fill url addresses

--- internal/repository/repository_model.go
+++ internal/repository/repository_model.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"miya/init/env"
 	"miya/internal/assignment"
 	"miya/internal/common"
 )
@@ -22,6 +23,14 @@ type Repository struct {
 	Users                map[string]common.User       `json:"users"`
 	Reviewers            map[string][]common.Reviewer `json:"reviewers"`
 	PRs                  []common.PullRequest         `json:"prs"`
+}
+
+func (repo *Repository) Initialize() {
+	// choose according provider Bitbucket
+	repo.FetchRepoUsersUrl = env.BitbucketFetchRepoUsersURL(repo.ProjectName, repo.Name)
+	repo.FetchProjectUsersUrl = env.BitbucketFetchProjectUsersURL(repo.ProjectName)
+	repo.FetchPrsUrl = env.BitbucketFetchPrListURL(repo.ProjectName, repo.Name)
+	repo.Token = env.BitbucketToken
 }
 
 func (repo *Repository) Assign() {
