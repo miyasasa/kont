8cf93dae950e39d3de8986df6f0c99e72c9d9a24
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-20 14:59:39 +0300

    refactored api.go

--- internal/remoterepository/bitbucket/api.go
+++ internal/remoterepository/bitbucket/api.go
@@ -3,7 +3,6 @@ package bitbucket
 import (
 	"bytes"
 	"encoding/json"
-	"kont/init/env"
 	"kont/internal/client"
 	"kont/internal/common"
 	"kont/internal/repository"
@@ -12,29 +11,33 @@ import (
 	"strconv"
 )
 
-func fetchUsers(url string, start int) []common.User {
+func fetchUsers(url string, token string, start int) []common.User {
 	req, _ := http.NewRequest("GET", url+"?start="+strconv.Itoa(start), nil)
-	req.Header.Add("Authorization", env.BitbucketToken)
+	req.Header.Add("Authorization", token)
 
 	page := common.UserPagination{}
 	client.GET(req, &page)
 
-	if page.IsLastPage {
+	if page.IsLastPage || page.NextPageStart == 0 {
 		return page.GetUsers()
 	}
 
-	return append(fetchUsers(url, page.NextPageStart), page.GetUsers()...)
+	return append(fetchUsers(url, token, page.NextPageStart), page.GetUsers()...)
 }
 
 // ignored pagination request cause of pageSize equals 25
-func fetchPRs(repo *repository.Repository) {
-	req, _ := http.NewRequest("GET", repo.FetchPrsUrl, nil)
+func fetchPRs(repo *repository.Repository, start int) []common.PullRequest {
+	req, _ := http.NewRequest("GET", repo.FetchPrsUrl+"?start="+strconv.Itoa(start), nil)
 	req.Header.Add("Authorization", repo.Token)
 
 	page := common.PRPagination{}
 	client.GET(req, &page)
 
-	repo.PRs = page.Values
+	if page.IsLastPage || page.NextPageStart == 0 {
+		return page.Values
+	}
+
+	return append(fetchPRs(repo, page.NextPageStart), page.Values...)
 }
 
 func updatePRs(repo *repository.Repository) {
