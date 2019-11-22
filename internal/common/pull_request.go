1a7450f02d47f42382971c1a0ee774af15d6b700
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-22 11:23:34 +0300

    moved functions and structs

--- internal/common/pull_request.go
+++ internal/common/pull_request.go
@@ -1,13 +1,46 @@
 package common
 
+import "github.com/deckarep/golang-set"
+
 type PullRequest struct {
 	Id          int32      `json:"id"`
 	Version     int32      `json:"version"`
 	Title       string     `json:"title"`
 	Description string     `json:"description"`
 	Reviewers   []Reviewer `json:"reviewers"`
+	Author      Author     `json:"author"`
+}
+
+type Reviewer struct {
+	User     User `json:"user"`
+	Approved bool `json:"approved"`
+}
+
+type User struct {
+	Name        string `json:"name"`
+	DisplayName string `json:"displayName"`
+}
+
+type Author struct {
+	User User `json:"user"`
+}
+
+func (a Author) GetAuthorAsReviewer() Reviewer {
+	return Reviewer{User: a.User}
 }
 
 func (pr *PullRequest) IsAssignedAnyReviewer() bool {
 	return len(pr.Reviewers) != 0
+}
+
+func (pr *PullRequest) GetReviewersByUnApproved() mapset.Set {
+	reviewers := mapset.NewSet()
+
+	for _, r := range pr.Reviewers {
+		if !r.Approved {
+			reviewers.Add(r)
+		}
+	}
+
+	return reviewers
 }
