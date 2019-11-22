7deb139ca06d917d6037044362b5de5213260004
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-11-22 11:23:59 +0300

    added RANDOMINAVAILABLE policy to assignent

--- internal/repository/assignment.go
+++ internal/repository/assignment.go
@@ -1,14 +1,17 @@
 package repository
 
 import (
+	"github.com/deckarep/golang-set"
 	"kont/internal/common"
+	"log"
 	"math/rand"
 	"time"
 )
 
 const (
-	FIRST  = "FIRST"
-	RANDOM = "RANDOM"
+	FIRST            = "FIRST"
+	RANDOM           = "RANDOM"
+	RANDOMINAVALABLE = "RANDOMINAVALABLE"
 )
 
 type Stage struct {
@@ -18,15 +21,24 @@ type Stage struct {
 	// added policy for assignment
 }
 
-func (s *Stage) GetReviewer() common.Reviewer {
+func (s *Stage) GetReviewerExcludesBusyReviewersAndOwner(busyReviewers mapset.Set, owner common.Reviewer) common.Reviewer {
+	log.Printf("BusyReviewers for per PR --> : %v and Owner--> %v", busyReviewers.ToSlice(), owner)
+
 	switch s.Policy {
 	case FIRST:
 		return s.getFirst()
 	case RANDOM:
 		return s.getRandomReviewer()
+	case RANDOMINAVALABLE:
+		return s.getRandomInAvailableReviewers()
 	default:
 		return s.getRandomReviewer()
 	}
+}
+
+func (s *Stage) getRandomInAvailableReviewers() common.Reviewer {
+
+	return common.Reviewer{}
 }
 
 func (s *Stage) getRandomReviewer() common.Reviewer {
