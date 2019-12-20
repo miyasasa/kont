ce77d965ccaf5557c1338f24d6e1773c4297a79a
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-12-20 08:32:35 +0300

    refactored code according DI refactored unit tests

--- internal/client/client.go
+++ internal/client/client.go
@@ -6,38 +6,39 @@ import (
 	"net/http"
 )
 
-func GET(req *http.Request, i interface{}) interface{} {
-
-	resp := send(req)
-
-	if resp != nil && resp.StatusCode == 200 {
-		err := json.NewDecoder(resp.Body).Decode(i)
-		if err != nil {
-			log.Printf("client::GET::Response Body can not be converted to desired type-struct, Req-Url: %s, Error: %s", req.URL, err)
-		}
-	} else {
-		log.Printf("client::GET::Unexpected response; StatusCode: %d, Req-Url: %s", resp.StatusCode, req.URL)
-	}
-
-	return i
+type Client interface {
+	GET(req *http.Request, i interface{})
+	PUT(req *http.Request)
 }
 
-func PUT(req *http.Request) {
-	resp := send(req)
+type HttpClient struct {
+	dispatcher Dispatcher
+}
 
-	if resp.StatusCode != 200 {
+func NewHttpClient(dispatcher Dispatcher) *HttpClient {
+	return &HttpClient{dispatcher: dispatcher}
+}
+
+func (c *HttpClient) GET(req *http.Request, i interface{}) {
+
+	resp := c.dispatcher.dispatch(req)
+
+	if resp != nil {
+		if resp.StatusCode == 200 {
+			err := json.NewDecoder(resp.Body).Decode(i)
+			if err != nil {
+				log.Printf("client::GET::Response Body can not be converted to desired type-struct, Req-Url: %s, Error: %s", req.URL, err)
+			}
+		} else {
+			log.Printf("client::GET::Unexpected response; StatusCode: %d, Req-Url: %v", resp.StatusCode, req.URL)
+		}
+	}
+}
+
+func (c HttpClient) PUT(req *http.Request) {
+	resp := c.dispatcher.dispatch(req)
+
+	if resp != nil && resp.StatusCode != 200 {
 		log.Printf("client::PUT:: PR can not updated URL: %s StatusCode: %v", req.URL, resp.StatusCode)
 	}
-}
-
-func send(req *http.Request) *http.Response {
-	client := &http.Client{}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Printf("An Error occur sen http-request to ... %s", err)
-		return nil
-	}
-
-	return resp
 }
--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -2,9 +2,12 @@ package client
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
-	"github.com/jarcoal/httpmock"
-	assert2 "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"io"
+	"io/ioutil"
 	"kont/internal/common"
 	"log"
 	"net/http"
@@ -12,51 +15,42 @@ import (
 	"testing"
 )
 
-func TestGET_Given200StatusCodeWithExpectedBody_ExpectToBindGivenInterface(t *testing.T) {
+func TestHttpClient_GET_Given200StatusCodeWithExpectedBody_ExpectToBindGivenInterface(t *testing.T) {
+	assert := assert.New(t)
 
 	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network/pull-requests?start=0"
-	assert := assert2.New(t)
-
-	//active http mock
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	//simulate response
-	expectedPage := common.PRPagination{Pagination: common.Pagination{Size: 10, Limit: 10, IsLastPage: true}}
-	responder, _ := httpmock.NewJsonResponder(200, &expectedPage)
-
-	httpmock.RegisterResponder("GET", prUrl, responder)
-
-	//get expected response
-	page := common.PRPagination{}
 	req, _ := http.NewRequest("GET", prUrl, nil)
-	GET(req, &page)
+	expectedPage := &common.PRPagination{Pagination: common.Pagination{Size: 10, Limit: 10, IsLastPage: true}}
+
+	dispatcherMock := HttpDispatcherMock{}
+	dispatcherMock.On("dispatch", req).Return(&http.Response{StatusCode: 200, Body: getBody(expectedPage)})
+
+	c := NewHttpClient(dispatcherMock)
+
+	page := common.PRPagination{}
+	c.GET(req, &page)
 
 	assert.NotNil(page)
-	assert.Equal(expectedPage.Size, page.Size)
-	assert.Equal(expectedPage.Limit, page.Limit)
+	assert.Equal(10, page.Size)
+	assert.Equal(10, page.Limit)
 	assert.True(page.IsLastPage)
 }
 
-func TestGET_Given200StatusCodeWithUnExpectedBody_ExpectEmptyGivenInterfaceAndErrorLog(t *testing.T) {
+func TestHttpClient_GET_Given200StatusCodeWithUnExpectedBody_ExpectEmptyGivenInterfaceAndErrorLog(t *testing.T) {
 
+	assert := assert.New(t)
 	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network/pull-requests?start=0"
-	assert := assert2.New(t)
-
-	//active http mock
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	//simulate response
-	responder, _ := httpmock.NewJsonResponder(200, "{\"repo:\":\"core-network\"}")
-
-	httpmock.RegisterResponder("GET", prUrl, responder)
-
-	//get expected response
-	page := common.PRPagination{}
 	req, _ := http.NewRequest("GET", prUrl, nil)
+	expected := "{\"repo:\":\"core-network\"}"
+
+	dispatcherMock := HttpDispatcherMock{}
+	dispatcherMock.On("dispatch", req).Return(&http.Response{StatusCode: 200, Body: getBody(expected)})
+
+	c := NewHttpClient(dispatcherMock)
+
+	page := common.PRPagination{}
 	output := captureOutput(func() {
-		GET(req, &page)
+		c.GET(req, &page)
 	})
 
 	assert.NotNil(page)
@@ -69,26 +63,21 @@ func TestGET_Given200StatusCodeWithUnExpectedBody_ExpectEmptyGivenInterfaceAndEr
 	assert.Contains(output, "Error: json: cannot unmarshal string into Go value of type common.PRPagination")
 }
 
-func TestGET_Given404_ExpectEmptyInterfaceAndErrorLog(t *testing.T) {
+func TestHttpClient_GET_Given404StatusCode_ExpectEmptyInterfaceAndErrorLog(t *testing.T) {
+	assert := assert.New(t)
 
 	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests?start=0"
-	assert := assert2.New(t)
-
-	//active http mock
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	//simulate response
-	responder, _ := httpmock.NewJsonResponder(404, "{\"errors\": [{\"context\": null,\"message\": \"Repository BESG/core-network1 does not exist.\",\"exceptionName\": \"com.atlassian.bitbucket.repository.NoSuchRepositoryException\"}]}")
-
-	httpmock.RegisterResponder("GET", prUrl, responder)
-
-	//get expected response
-	page := common.PRPagination{}
 	req, _ := http.NewRequest("GET", prUrl, nil)
+	expected := "{\"errors\": [{\"context\": null,\"message\": \"Repository BESG/core-network1 does not exist.\",\"exceptionName\": \"com.atlassian.bitbucket.repository.NoSuchRepositoryException\"}]}"
 
+	dispatcherMock := HttpDispatcherMock{}
+	dispatcherMock.On("dispatch", req).Return(&http.Response{StatusCode: 404, Body: getBody(expected)})
+
+	c := NewHttpClient(dispatcherMock)
+
+	page := common.PRPagination{}
 	output := captureOutput(func() {
-		GET(req, &page)
+		c.GET(req, &page)
 	})
 
 	assert.NotNil(page)
@@ -98,6 +87,104 @@ func TestGET_Given404_ExpectEmptyInterfaceAndErrorLog(t *testing.T) {
 
 	assert.NotNil(output)
 	assert.Contains(output, fmt.Sprintf("client::GET::Unexpected response; StatusCode: 404, Req-Url: %s", prUrl))
+}
+
+func TestHttpClient_GET_GivenNilByDispatcher_ExpectEmptyPrPagination(t *testing.T) {
+	assert := assert.New(t)
+
+	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests?start=0"
+	req, _ := http.NewRequest("GET", prUrl, nil)
+
+	dispatcherMock := HttpDispatcherMock{}
+	dispatcherMock.On("dispatch", req).Return(nil)
+
+	c := NewHttpClient(dispatcherMock)
+
+	page := common.PRPagination{}
+	output := captureOutput(func() {
+		c.GET(req, &page)
+	})
+
+	assert.NotNil(page)
+	assert.Equal(0, page.Size)
+	assert.Equal(0, page.Limit)
+	assert.False(page.IsLastPage)
+
+	assert.Empty(output)
+}
+
+func TestHttpClient_PUT_Given200ResponseMock_NotExpectError(t *testing.T) {
+	assert := assert.New(t)
+
+	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests/1903"
+	req, _ := http.NewRequest("PUT", prUrl, nil)
+
+	dispatcherMock := HttpDispatcherMock{}
+	dispatcherMock.On("dispatch", req).Return(&http.Response{StatusCode: 200})
+
+	c := NewHttpClient(dispatcherMock)
+	output := captureOutput(func() {
+		c.PUT(req)
+	})
+
+	assert.Empty(output)
+
+}
+
+func TestHttpClient_PUT_Given500ResponseMock_ExpectErrorLog(t *testing.T) {
+	assert := assert.New(t)
+
+	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests/1903"
+	req, _ := http.NewRequest("PUT", prUrl, nil)
+
+	dispatcherMock := HttpDispatcherMock{}
+	dispatcherMock.On("dispatch", req).Return(&http.Response{StatusCode: 500})
+
+	c := NewHttpClient(dispatcherMock)
+	output := captureOutput(func() {
+		c.PUT(req)
+	})
+
+	assert.NotNil(output)
+	assert.Contains(output, fmt.Sprintf("client::PUT:: PR can not updated URL: %s StatusCode: 500", prUrl))
+}
+
+func TestHttpClient_PUT_GivenNilResponseMock_NotExpectError(t *testing.T) {
+	assert := assert.New(t)
+
+	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests/1903"
+	req, _ := http.NewRequest("PUT", prUrl, nil)
+
+	dispatcherMock := HttpDispatcherMock{}
+	dispatcherMock.On("dispatch", req).Return(nil)
+
+	c := NewHttpClient(dispatcherMock)
+	output := captureOutput(func() {
+		c.PUT(req)
+	})
+
+	assert.Empty(output)
+}
+
+type HttpDispatcherMock struct {
+	mock.Mock
+}
+
+func (d HttpDispatcherMock) dispatch(req *http.Request) *http.Response {
+	args := d.Called(req)
+
+	if args.Get(0) == nil {
+		return nil
+	}
+
+	return args.Get(0).(*http.Response)
+}
+
+func getBody(p interface{}) io.ReadCloser {
+	requestByte, _ := json.Marshal(p)
+	requestReader := bytes.NewReader(requestByte)
+
+	return ioutil.NopCloser(requestReader)
 }
 
 func captureOutput(f func()) string {
