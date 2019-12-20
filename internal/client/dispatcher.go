b50f67e36bc30a44f8f1654ae18324582c83e2b4
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2019-12-20 08:33:48 +0300

    separated send method as dispatcher service added unit tests

---
+++ internal/client/dispatcher.go
@@ -0,0 +1,24 @@
+package client
+
+import (
+	"log"
+	"net/http"
+)
+
+type Dispatcher interface {
+	dispatch(req *http.Request) *http.Response
+}
+
+type HttpDispatcher struct{}
+
+func (d HttpDispatcher) dispatch(req *http.Request) *http.Response {
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Printf("Dispatcher::dispatch An Error occur send http-request to:  %s", err)
+		return nil
+	}
+
+	return resp
+}
---
+++ internal/client/dispatcher_test.go
@@ -0,0 +1,107 @@
+package client
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestGET_Given200ResponseToResponder_Expect200AsResponse(t *testing.T) {
+
+	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests?start=0"
+	assert := assert.New(t)
+
+	//active http mock
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	//simulate response
+	body := "Güven Yalçın"
+	responder := httpmock.NewStringResponder(200, body)
+
+	httpmock.RegisterResponder("PUT", prUrl, responder)
+
+	d := HttpDispatcher{}
+
+	//get expected response
+	req, _ := http.NewRequest("PUT", prUrl, nil)
+	resp := d.dispatch(req)
+
+	assert.NotNil(resp)
+	assert.Equal(200, resp.StatusCode)
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(err)
+	assert.Equal(body, string(bodyBytes))
+
+}
+
+func TestGET_Given404ResponseToResponder_Expect404AsResponse(t *testing.T) {
+
+	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests?start=0"
+	assert := assert.New(t)
+
+	//active http mock
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	//simulate response
+	body := `{"errors": [{"context": null,"message": "Repository BESG/core-network1 does not exist.","exceptionName": "com.atlassian.bitbucket.repository.NoSuchRepositoryException"}]}`
+
+	responder, _ := httpmock.NewJsonResponder(404, body)
+	httpmock.RegisterResponder("PUT", prUrl, responder)
+
+	d := HttpDispatcher{}
+
+	//get expected response
+	req, _ := http.NewRequest("PUT", prUrl, nil)
+	resp := d.dispatch(req)
+
+	assert.NotNil(resp)
+	assert.Equal(404, resp.StatusCode)
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(err)
+
+	var result string
+	err = json.Unmarshal(bodyBytes, &result)
+	assert.Nil(err)
+
+	assert.Equal(body, result)
+}
+
+func TestGET_GivenNilResponseToResponder_ExpectNilAsResponseAndErrorLogOnConsole(t *testing.T) {
+
+	prUrl := "http://localhost:7990/rest/api/1.0/projects/BESG/repos/core-network1/pull-requests?start=0"
+	assert := assert.New(t)
+
+	//active http mock
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	//simulate response
+	req, _ := http.NewRequest("PUT", prUrl, nil)
+
+	responder := func(req *http.Request) (*http.Response, error) {
+		return nil, errors.New("request error")
+	}
+
+	httpmock.RegisterResponder("PUT", prUrl, responder)
+
+	d := HttpDispatcher{}
+
+	resp := &http.Response{}
+	assert.NotNil(resp)
+
+	output := captureOutput(func() {
+		resp = d.dispatch(req)
+	})
+
+	assert.Nil(resp)
+	assert.Contains(output, fmt.Sprintf("Dispatcher::dispatch An Error occur send http-request to:  Put %s: request error", prUrl))
+}
