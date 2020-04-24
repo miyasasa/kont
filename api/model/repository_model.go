43195e76c17dc06ea54b8dad8bde71890add43b4
Yasin KIZILKAYA <ykizilkaya@havelsan.com.tr>
2020-04-24 13:47:11 +0300

    moved the mapper(Repository -> RepositoryModel) methods to repository_model file. deleted repository_model_mapper file

--- api/model/repository_model.go
+++ api/model/repository_model.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"kont/internal/common"
+	"kont/internal/repository"
 )
 
 type RepositoryModel struct {
@@ -24,4 +25,43 @@ type StageModel struct {
 type ReviewerModel struct {
 	Priority int         `json:"priority"`
 	User     common.User `json:"user"`
+}
+
+func MapRepositoryToRepositoryModel(repo *repository.Repository) *RepositoryModel {
+	var repoModel = &RepositoryModel{}
+	repoModel.Host = repo.Host
+	repoModel.ProjectName = repo.ProjectName
+	repoModel.Name = repo.Name
+	repoModel.DevelopmentBranch = repo.DevelopmentBranch
+	repoModel.Provider = repo.Provider
+	repoModel.DefaultComment = repo.DefaultComment
+	repoModel.Users = repo.Users
+
+	stageModels := make([]StageModel, 0, len(repo.Stages))
+	for _, s := range repo.Stages {
+		sm := MapStageModel(s)
+		stageModels = append(stageModels, sm)
+	}
+
+	repoModel.StageModel = stageModels
+
+	return repoModel
+}
+
+func MapStageModel(stage repository.Stage) StageModel {
+	var stageModel = StageModel{Name: stage.Name, Policy: stage.Policy}
+
+	reviewerModels := make([]ReviewerModel, 0, len(stage.Reviewers))
+	for _, r := range stage.Reviewers {
+		rm := MapReviewerModel(r)
+		reviewerModels = append(reviewerModels, rm)
+	}
+
+	stageModel.ReviewerModel = reviewerModels
+
+	return stageModel
+}
+
+func MapReviewerModel(reviewer *common.Reviewer) ReviewerModel {
+	return ReviewerModel{Priority: reviewer.Priority, User: reviewer.User}
 }
